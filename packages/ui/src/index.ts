17b4543b8f3d9a7d6bb7a99a53f3199ede75fee5
Anthony Liveris <amliveris@gmail.com>
2026-05-01 17:04:17 +1000

    Add unified hamburger SiteMenu site-wide; build /portfolio index; fix logo SVG fills; hide active item from menu

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,5 @@
 export { default as Container } from './components/Container.astro';
 export { default as Nav } from './components/Nav.astro';
+export { default as SiteMenu } from './components/SiteMenu.astro';
 export { default as Footer } from './components/Footer.astro';
 export { default as Button } from './components/Button.astro';
