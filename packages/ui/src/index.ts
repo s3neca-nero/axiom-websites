b78bd830d798e8e1c4b5df05f065c80af310cbeb
Anthony Liveris <amliveris@gmail.com>
2026-05-03 17:38:52 +1000

    Thesis polish; sitewide HelixMotif; interactive home splash (cursor glow + click pulse)

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,3 +3,4 @@ export { default as Nav } from './components/Nav.astro';
 export { default as SiteMenu } from './components/SiteMenu.astro';
 export { default as Footer } from './components/Footer.astro';
 export { default as Button } from './components/Button.astro';
+export { default as HelixMotif } from './components/HelixMotif.astro';
